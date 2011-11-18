ca822d20c0c21a070cda854609596da301782437
suraj chawda <kingsuru@gmail.com>
2011-11-18 16:34:37 +0000

    init

---
+++ TEST.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Suraj</title>
+</head>
+
+<body bgcolor="#333333"><table width="100%" height="200" border="0" bgcolor="#999999">
+  <tr>
+    <td align="center" valign="middle" bgcolor="#666666"><p>Your query string is<strong> <?php 
+	$query=$_SERVER['QUERY_STRING'];
+echo $query;
+?></strong></p></td>
+  </tr>
+</table>
+
+
+</body>
+</html>
